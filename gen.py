c0023968701de9183895ed3b5e751f9bd0e6b88a
Ruoshan Huang <ruoshan612@gmail.com>
2015-03-31 00:35:29 +0800

    initial commit

---
+++ gen.py
@@ -0,0 +1,32 @@
+#!/usr/local/bin/python
+
+import os, re, sqlite3
+from bs4 import BeautifulSoup, NavigableString, Tag 
+
+db = sqlite3.connect('./docSet.dsidx')
+cur = db.cursor()
+
+try:
+    cur.execute('DROP TABLE searchIndex;')
+except:
+    pass
+
+cur.execute('CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);')
+cur.execute('CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);')
+
+docpath = './Documents'
+
+page = open(os.path.join(docpath,'configuration.html')).read()
+soup = BeautifulSoup(page)
+
+for div in soup.find_all('div', {'class': "keyword"}):
+    try:
+        a = div.b.find_all("a")[1]
+        name = a.string
+        path = "configuration.html" + a.attrs["href"]
+        cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, 'Directive', path))
+    except:
+        continue
+
+db.commit()
+db.close()
---
+++ readme.md
@@ -0,0 +1,8 @@
+## How to generate the docset
+
+The html, js, css and font are downloaded from [here](http://cbonte.github.io/haproxy-dconv/index.html).
+I remove the navabar, search box and some fixup to make them offline.
+
+Use the script `gen.py` under the `Documents` directory to generate the right dosSet.dsidx.
+
+There would be all.
